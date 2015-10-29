839a172266987212b4c519cc4b19532e4a323a73
Vagner Araujo <vagnerarau@gmail.com>
2015-10-29 19:50:00 -0200

    ajustes

--- www/js/welcomeController.js
+++ www/js/welcomeController.js
@@ -82,6 +82,7 @@ angular.module('starter')
 	// Metodo que alimenta a variaveis com o JSON gerado na hora do login ou Refresh
 	this.setData = function(){ 
 		console.log(" chamada ao JSON Login");
+		
 		$http.get('http://app.rjag.com.br/app-IOS/login.json')			
 		 .success(function(data, status, headers, config) {
 			 datas = data;
